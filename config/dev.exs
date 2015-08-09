ddcb4c15ecfe2074c2deaf814cfec52f26f29a03
Juan Pastas <pastasjuan@gmail.com>
2015-08-08 23:43:51 -0500

    mix get.deps

--- config/dev.exs
+++ config/dev.exs
@@ -29,7 +29,7 @@ config :logger, :console, format: "[$level] $message\n"
 # Configure your database
 config :phoenix_test, PhoenixTest.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "phoenix_test_dev",
   size: 10 # The amount of database connections in the pool
